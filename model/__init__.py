3b9c711179e2ed1c68adda6c9d7798b3b8573761
Xiaowei Ou <xo24@grace2.grace.hpc.yale.internal>
2023-04-09 17:04:53 -0400

    primitive model submodule

---
+++ model/__init__.py
@@ -0,0 +1,2 @@
+from .model import Model
+from .mlp_complex_tanh import MLPComplexTanh
---
+++ model/model.py
@@ -0,0 +1,31 @@
+class Model(object):
+
+    def __init__(self, num_visible, num_hidden=[256]):
+        self.num_visible = 0
+        self.num_visible = num_visible
+        self.num_hidden = num_hidden
+        self.num_layer = len(num_hidden)
+
+    def log_val(self, v):
+        pass
+
+    def log_val_diff(self, v1, v2):
+        pass
+
+    def derlog(self, v, size):
+        pass
+
+    def get_parameters(self):
+        pass
+
+    def visualize_param(self):
+        pass
+
+    def is_real(self):
+        return False
+
+    def is_complex(self):
+        return False
+
+    def is_probability(self):
+        return False
