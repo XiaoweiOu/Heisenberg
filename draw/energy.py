4dd97a6eb989f0dd05dd656128f6bc9fba7d17e5
Xiaowei Ou <xo24@grace2.grace.hpc.yale.internal>
2023-05-14 15:01:10 -0400

    update draw/energy.py

--- draw/energy.py
+++ draw/energy.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
 
-result_file = 'result.csv'
+result_file = './result/CNN_MSR_SUN6_2000sample_test3_energy.csv'
 epoch = []
 energy = []
 with open(result_file,'r') as csvfile:
@@ -10,6 +11,8 @@ with open(result_file,'r') as csvfile:
     for row in csvreader:
         epoch.append(int(row[0]))
         energy.append(float(row[1]))
+
+print("Average energy in the last 20 iterations:",np.mean(energy[-20:]))
 
 plt.plot(energy)
 plt.xlabel('epoch')
