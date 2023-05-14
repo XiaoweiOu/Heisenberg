f6011abc877fe029d66ab7dbff067f9cfb4dce3c
Xiaowei Ou <xo24@grace2.grace.hpc.yale.internal>
2023-05-14 15:20:02 -0400

    update sampler

--- sampler/sampler.py
+++ sampler/sampler.py
@@ -1,12 +1,13 @@
 class Sampler(object):
 
-    def __init__(self, num_samples):
+    def __init__(self, num_samples, graph):
         self.num_samples = num_samples
+        self.graph = graph
 
     def get_initial_random_samples(self, sample_size, num_samples=None):
         pass
 
-    def sample(self, machine, initial_sample, num_samples):
+    def sample(self, machine, initial_sample, num_samples, num_steps):
         pass
 
     def set_num_samples(self, num_samples):
